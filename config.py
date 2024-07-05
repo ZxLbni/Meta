6991a6b5629c1382884b0231e1165bc535a276a6
⤷ßñï 🪽 <101978891+ZxLbni@users.noreply.github.com>
2024-07-05 09:55:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,17 +6,14 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = int(os.environ.get("API_ID", "10811400"))
-API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAGjSKZOl_kPksvKJNaKbiBl5XDI0tt5QpY")
-ADMIN = int(os.environ.get("ADMIN", '6469754522')) 
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
+API_ID = int(os.environ.get("API_ID", "22419004"))
+API_HASH = os.environ.get("API_HASH", "34982b52c4a83c2af3ce8f4fe12fe4e1")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7313161759:AAEf5yfyfThMxRInmJLbpp-lOFq3Hx8nF3I")
+ADMIN = int(os.environ.get("ADMIN", '6742022802')) 
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "NOOBPrivate")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "NOOBPrivateChats")
 CAPTION = os.environ.get("CAPTION", "")
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 DOWNLOAD_LOCATION = "./DOWNLOADS"
-SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
-
-
-
+SUNRISES_PIC= "https://telegra.ph/file/3999ba9e4ce962ff15a78.jpg"  # Replace with your Telegraph link
+AUTH_USERS = int(os.environ.get("AUTH_USERS", '6742022802'))
